e26bc59bf140b02f3719f3e78f002568993b7d88
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-11-04 18:27:32 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+  # get 'posts', to: 'posts#checked'   queryパラメーターの書き方
+  get 'posts/:id', to: 'posts#checked' #pathパラメーターの書き方
  end
